33b52b5b75451d0f618cd033a97ea02810f6af4f
TemaSir <tianxin@gsoft.cc>
2017-02-27 16:10:18 +0800

    Revert "update"
    This reverts commit d89a423d3a4231e04668032bed9de834d34ad307.

--- weibo/AppDelegate.swift
+++ weibo/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        setRootViewController()
+        // Override point for customization after application launch.
         return true
     }
 
@@ -41,12 +41,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-    fileprivate func setRootViewController (){
-        self.window = UIWindow(frame: UIScreen.main.bounds)
-        self.window?.backgroundColor = UIColor.white
-        self.window?.rootViewController = DTRootViewController()
-        self.window?.makeKeyAndVisible()
-    }
-    
+
 }
 

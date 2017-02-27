910e6d3383d0ea8dda4a996cc789bd9d0bd7b41d
Tema <18320686151@163.com>
2017-02-27 23:12:51 +0800

    添加文件

---
+++ weibo/DTBaseTableViewViewController.swift
@@ -0,0 +1,23 @@
+//
+//  DTBaseTableViewViewController.swift
+//  weibo
+//
+//  Created by Tema on 17/2/27.
+//  Copyright © 2017年 tianXin. All rights reserved.
+//
+
+import UIKit
+
+class DTBaseTableViewViewController: DTViewController {
+
+    lazy var tableView = UITableView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+ 
+
+}

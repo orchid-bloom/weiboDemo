6d1da101e921d6fd921e1ccda167de54837291f7
TemaSir <tianxin@gsoft.cc>
2017-03-01 15:25:38 +0800

    update

---
+++ weibo/DTAppStyle.swift
@@ -0,0 +1,14 @@
+//
+//  DTAppStyle.swift
+//  weibo
+//
+//  Created by tianXin on 2017/3/1.
+//  Copyright © 2017年 tianXin. All rights reserved.
+//
+
+import Foundation
+import EDColor
+
+extension UIColor {
+    
+}
---
+++ weibo/DTFont.swift
@@ -0,0 +1,43 @@
+//
+//  DTFont.swift
+//  weibo
+//
+//  Created by tianXin on 2017/3/1.
+//  Copyright © 2017年 tianXin. All rights reserved.
+//
+
+import UIKit
+
+enum DTFontType {
+    case DTFontTypeA /// 导航栏标题
+    case DTFontTypeB /// 标题
+    case DTFontTypeC /// 内容
+    case DTFontTypeD /// 小号字
+    case DTFontTypeE /// 列表标记行专用
+}
+enum DTTitleFontType {
+    case DTTitleFontTypeA  /// 导航栏标题
+    case DTTitleFontTypeB  /// 标题
+    case DTTitleFontTypeC  /// 内容
+}
+
+class DTFont: UIFont {
+
+    class func getFontWithFontType(fontType:DTFontType) -> UIFont {
+        var font:UIFont
+        switch fontType {
+        case .DTFontTypeA:
+            font = UIFont.systemFont(ofSize: 38/2)
+        case .DTFontTypeB:
+            font = UIFont.systemFont(ofSize: 34/2)
+        case .DTFontTypeC:
+            font = UIFont.systemFont(ofSize: 30/2)
+        case .DTFontTypeD:
+            font = UIFont.systemFont(ofSize: 26/2)
+        case .DTFontTypeE:
+            font = UIFont.systemFont(ofSize: 20/2)
+        }
+        return font
+    }
+    
+}

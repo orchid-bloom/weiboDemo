1a445410385050aa32ac239ead85b7ea0a4874e9
TemaSir <tianxin@gsoft.cc>
2017-03-01 11:32:44 +0800

    初始化list

--- weibo/DTPage.swift
+++ weibo/DTPage.swift
@@ -9,8 +9,30 @@
 import UIKit
 
 class DTPage: NSObject {
-
+    ///当前页数
     var pageIndex = 1
+    ///每页数目
     var pageSize  = 10
-    var totalPages = 0
+    ///总页数
+    var _totalPages :Int = 0
+    var totalPages  :Int { //重写set方法
+        set {
+            if newValue > pageIndex {
+                if newValue%pageSize == 0 {
+                    _totalPages = newValue/pageSize
+                }else{
+                    _totalPages = newValue/pageSize+1
+                }
+            }else{
+                _totalPages = 1
+            }
+        }
+        get {
+            return _totalPages
+        }
+    }
+    //增加页数
+    func addPageIndex()  {
+        pageIndex += 1
+    }
 }
--- weibo/DTPaginationViewController.swift
+++ weibo/DTPaginationViewController.swift
@@ -7,6 +7,12 @@
 //
 
 import UIKit
+import MJRefresh
+
+enum RefreshType {
+    case  HeaderRefresh
+    case  FooterRefresh
+}
 
 class DTPaginationViewController: DTViewController {
 
@@ -18,6 +24,67 @@ class DTPaginationViewController: DTViewController {
         super.viewDidLoad()
 
     }
-
-
+    //添加下拉刷新
+    func addHeadRefresh() {
+        let headRefresh = MJRefreshHeader(refreshingTarget: self, refreshingAction: #selector(headerRefreshing(sender:)))
+        refreshTableView.mj_header = headRefresh
+    }
+    //添加上托加载更多
+    func addFooterRefresh() {
+        let footRefresh = MJRefreshFooter(refreshingTarget: self, refreshingAction: #selector(footRefreshing(sender:)))
+        refreshTableView.mj_footer = footRefresh
+    }
+    //刷新
+    func headerRefreshing(sender :Any) {
+        beforeStartRefresh()
+        page.pageIndex = 1
+        requestWithPageIndex(pageIndex: page.pageIndex, refreshType: .HeaderRefresh)
+    }
+    //加载更多
+    func footRefreshing(sender :Any) {
+        beforeStartRefresh()
+        requestWithPageIndex(pageIndex: page.pageIndex, refreshType: .FooterRefresh)
+    }
+    //结束刷新
+    func endRefreshingWithType(refreshType:RefreshType)  {
+        
+    }
+    //子类需要重写这个方法
+    func requestWithPageIndex(pageIndex:Int,refreshType:RefreshType) {
+        loadWithPage(pageIndex: pageIndex, requestType: refreshType)
+    }
+    /**
+     *  加载分页, 该方法需要子类重写，此方法废弃。使用requestWithPageIndex
+     *
+     *  @param pageIndex 当前分页索引
+     *  @param requestType 请求类型
+     */
+    func loadWithPage(pageIndex:Int,requestType:RefreshType) {
+        
+    }
+    /**
+     *  添加数据前调用，处理下拉和加载更多的结束后的动作
+     *
+     *  @param refreshType 刷新类型
+     *  @param myDataList  添加数据
+     */
+    func addDataBeforeWithRefreshType(refreshType:RefreshType,dataArray:[Any])  {
+        if refreshType == .HeaderRefresh  {
+            dataList.removeAll()
+            if dataArray.count > page.pageSize {
+                page.addPageIndex()
+                addFooterRefresh()
+            }else{
+                refreshTableView.mj_footer = nil
+            }
+        }else{
+            page.addPageIndex()
+            if dataArray.count < page.pageSize {
+                refreshTableView.mj_footer = nil
+            }
+        }
+    }
+    func beforeStartRefresh() {
+    
+    }
 }

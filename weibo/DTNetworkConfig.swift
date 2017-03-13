fc50351a00bdbe77e87a3a0d863fee32b6e67cd4
TemaSir <tianxin@gsoft.cc>
2017-03-13 17:29:59 +0800

    baseUrl封装

--- weibo/DTNetworkConfig.swift
+++ weibo/DTNetworkConfig.swift
@@ -36,6 +36,7 @@ public final class DTNetworkConfig: NSObject {
         case .SeverRELEASE:
             baseUrl = ""
        }
+        baseUrl = "https://api.weibo.com"
         return baseUrl!
     }
 }

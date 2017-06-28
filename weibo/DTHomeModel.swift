4b09e8213c2d9e0d645bacedec2191357c7e914d
Tema <18320686151@163.com>
2017-06-28 21:20:45 +0800

    update

--- weibo/DTHomeModel.swift
+++ weibo/DTHomeModel.swift
@@ -29,16 +29,6 @@ class DTHomeModel: DTModel {
     var titlepic: String?
     /// 多图数组
     var morepic: [String]?
-//    /// 缓存行高
-//    var rowHeight: CGFloat = 0.0
-//    /// 缓存偏移量
-//    var offsetY: CGFloat = 0.0
-    /// 时间戳转换成时间
-//    var newstimeString: String?
-//    {
-//        return newstime!.timeStampToString()
-//    }
-
 }
 
 //MARK: - MTLJSONSerializing -
@@ -46,26 +36,16 @@ extension DTHomeModel : MTLJSONSerializing {
 
     class func jsonKeyPathsByPropertyKey() -> [AnyHashable : Any]! {
         let dict = [
-        "classid":"classid",
-        "id":"id",
-        "title":"title",
-        "befrom":"befrom",
-        "onclick":"onclick",
-        "plnum":"plnum",
+        "classid" :"classid",
+        "id"      :"id",
+        "title"   :"title",
+        "befrom"  :"befrom",
+        "onclick" :"onclick",
+        "plnum"   :"plnum",
         "newstime":"newstime",
         "titlepic":"titlepic",
-        "morepic":"morepic",
+        "morepic" :"morepic",
         ]
         return dict
     }
-//    class   func jsonTransformer(forKey key: String!) -> ValueTransformer! {
-//        if key == "user" {
-////            return MTLJSONAdapter.arrayTransformer(withModelClass: DTUser.self)
-//            return nil
-//        }else if key == "DTStatusPicture" {
-////            return MTLJSONAdapter.arrayTransformer(withModelClass: DTStatusPicture.self)
-//            return nil
-//        }
-//        return nil
-//    }
 }
